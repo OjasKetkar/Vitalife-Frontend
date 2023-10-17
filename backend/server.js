1f5469b946f474bb3e265a346a1c6e1a9d6cb967
Ojas <ketkarojas@gmail.com>
2023-10-18 02:34:46 +0530

    Cors error

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,16 @@ const colors = require('colors');
 const port = process.env.PORT || 8000;
 const errorHandler = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require('cors'); 
 
 connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
